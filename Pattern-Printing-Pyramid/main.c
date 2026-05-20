62eef23a1388fd269af86ca8eca8a962935f42f2
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-05-20 20:16:25 +0600

    add pattern printing pyramid

---
+++ Pattern-Printing-Pyramid/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    if (rows <= 0)
+    {
+        printf("Error ! Please enter a positive value");
+    }
+
+    else
+    {
+        for (int i = 1; i <= rows; i++)
+        {
+
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++)
+            {
+                printf(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                printf("*");
+            }
+
+            printf("\n");
+        }
+    }
+    return 0;
+}
